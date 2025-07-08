f483f460fa30b4481c7fe8a56d07967b36f70ecb
dvtinh99 <tinh.doan@codecomplete.jp>
2025-07-08 15:59:36 +0700

    update info

--- pages/projects.vue
+++ pages/projects.vue
@@ -80,7 +80,7 @@ import DevConfig from '~/developer.json';
 
 const config = ref(DevConfig)
 
-const techs = ['React', 'HTML', 'CSS', 'Vue', 'Angular', 'Gatsby', 'Flutter']
+const techs = ['HTML', 'CSS', 'Vue', 'PHP', 'Laravel', 'Linux']
 const filters = ref(['all'])
 const showFilters = ref(true)
 const projects = ref(config.value.projects)
