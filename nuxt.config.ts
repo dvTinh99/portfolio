95b837c5578bf75f29b158c954b0a37a3f9b3dc9
dvtinh99 <tinh.doan@codecomplete.jp>
2025-07-08 17:03:08 +0700

    update base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,6 @@ const siteTitle = `${config.name} | ${config.role}`
  Usage: https://nuxt.com/docs/api/configuration/nuxt-config
  */
 export default defineNuxtConfig({
-  base: 'portfolio',
   compatibilityDate: '2025-02-28',
   devtools: { enabled: true },
   /**
@@ -42,6 +41,7 @@ export default defineNuxtConfig({
         { rel: 'apple-touch-icon', href: 'pwa/icons/apple-touch-icon.png' },
       ],
     },
+    baseURL: '/portfolio',
   },
 
   /**
